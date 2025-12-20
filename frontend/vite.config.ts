4f0554af38c5b2a3716cef0ef8f0fe140cafafa0
KiraKiraAyu <kirakiraayu@outlook.com>
2025-12-20 21:09:11 +0800

    chore(frontend): configure vite development proxy server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
@@ -8,23 +8,36 @@ import Icons from 'unplugin-icons/vite'
 import Components from 'unplugin-vue-components/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-    Components({
-      resolvers: [IconsResolver()],
-    }),
-    Icons({ autoInstall: true }),
-  ],
-  
-  envDir: '../', 
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '../', '')
+  const backUrl = env.VITE_API_URL
 
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+      tailwindcss(),
+      Components({
+        resolvers: [IconsResolver()],
+      }),
+      Icons({ autoInstall: true }),
+    ],
+
+    envDir: '../',
+
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+
+    server: {
+      proxy: {
+        '/api': {
+          target: backUrl,
+          changeOrigin: true,
+        },
+      }
+    }
+  }
 })
